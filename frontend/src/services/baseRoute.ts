51e2dd02a88addf6c03b926fcf1009033801db90
new_horizon <krishna.docs.api@gmail.com>
2024-06-18 06:43:24 +0200

    removed comment

--- frontend/src/services/baseRoute.ts
+++ frontend/src/services/baseRoute.ts
@@ -2,5 +2,3 @@ import { EnvironmentProvider } from '../core/environment';
 
 
 export const baseServerRoute: string = `${EnvironmentProvider.BASE_SERVER_URL}` as const;
-
-// export const baseServerRoute: string = 'http://0.0.0.0:8000/api' as const;
